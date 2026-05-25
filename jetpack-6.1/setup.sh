9a952ef99f3ecb385a34723f20c3192c174b5f87
Brendan Richards <quend00@gmail.com>
2026-05-25 11:38:43 -0400

    Update setup.sh

--- jetpack-6.1/setup.sh
+++ jetpack-6.1/setup.sh
@@ -27,3 +27,24 @@ sudo apt update && sudo apt install -y docker.io nvidia-container-toolkit && \
 
 
 # docker run --network host --runtime nvidia nvcr.io/nvidia/l4t-base:r36.2.0 nvidia-smi
+
+# test dual cams
+# gst-launch-1.0 \
+#   nvcompositor name=comp \
+#     sink_0::xpos=0 \
+#     sink_0::ypos=0 \
+#     sink_0::width=1280 \
+#     sink_0::height=720 \
+#     sink_1::xpos=1280 \
+#     sink_1::ypos=0 \
+#     sink_1::width=1280 \
+#     sink_1::height=720 \
+#   ! 'video/x-raw(memory:NVMM),width=2560,height=720,framerate=30/1' \
+#   ! nvvidconv \
+#   ! xvimagesink sync=false \
+#   nvarguscamerasrc sensor-id=0 \
+#   ! 'video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1' \
+#   ! comp.sink_0 \
+#   nvarguscamerasrc sensor-id=1 \
+#   ! 'video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1' \
+#   ! comp.sink_1
